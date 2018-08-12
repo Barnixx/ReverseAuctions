5f9ac508a33d0303865086a041045df6b0edead6
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-08-12 22:38:27 +0200

    create userRole class and change category class

--- src/main/java/pl/reverseAuctions/model/category/Category.java
+++ src/main/java/pl/reverseAuctions/model/category/Category.java
@@ -1,4 +1,41 @@
 package pl.reverseAuctions.model.category;
 
 public class Category {
+
+    private Long id;
+    private String categoryName;
+    private String categoryDescription;
+
+    public Category() {
+    }
+
+    public Category(Long id, String categoryName, String categoryDescription) {
+        this.id = id;
+        this.categoryName = categoryName;
+        this.categoryDescription = categoryDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
 }
