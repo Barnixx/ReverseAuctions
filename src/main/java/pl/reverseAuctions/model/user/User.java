d68909e0e009c44546689e3fdec1be4068698d84
Łukasz Rau <luk.rau.01@gmail.com>
2018-08-13 15:34:36 +0200

    User class and User DAO added

--- src/main/java/pl/reverseAuctions/model/user/User.java
+++ src/main/java/pl/reverseAuctions/model/user/User.java
@@ -1,7 +1,28 @@
 package pl.reverseAuctions.model.user;
 
+import java.sql.Date;
+
 public class User {
     private Long idUser;
+    private String login;
+    private String firstName;
+    private String lastName;
+    private Date birth;
+    private String mail;
+    private UserRole userRole;
+
+    public User() {
+    }
+
+    public User(Long idUser, String login, String firstName, String lastName, Date birth, String mail, UserRole userRole, UserStat userStat) {
+        this.idUser = idUser;
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birth = birth;
+        this.mail = mail;
+        this.userRole = userRole;
+    }
 
     public Long getIdUser() {
         return idUser;
@@ -10,4 +31,53 @@ public class User {
     public void setIdUser(Long idUser) {
         this.idUser = idUser;
     }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public UserRole getUserRole() {
+        return idUserRole;
+    }
+
+    public void setUserRole(UserRole idUserRole) {
+        this.idUserRole = idUserRole;
+    }
+
 }
