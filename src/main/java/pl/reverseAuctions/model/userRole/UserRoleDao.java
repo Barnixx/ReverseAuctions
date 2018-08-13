637d4843da518317f643bbc8fc4d55821bd1bf3b
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-08-13 15:52:05 +0200

    Category and UserRole + both DAO done

--- src/main/java/pl/reverseAuctions/model/userRole/UserRoleDao.java
+++ src/main/java/pl/reverseAuctions/model/userRole/UserRoleDao.java
@@ -22,22 +22,65 @@ public class UserRoleDao implements Entity<UserRole> {
 
     @Override
     public void saveToDb(UserRole model) throws SQLException {
-
+        try (Connection conn = DbUtil.getConn()){
+            if(model.getId() == null){
+                try (PreparedStatement preparedStatement = conn.prepareStatement(INSERT_QUERY)){
+                    preparedStatement.setString(1, model.getUserRoleName());
+                    preparedStatement.executeUpdate();
+                }
+            } else {
+                try (PreparedStatement preparedStatement = conn.prepareStatement(UPDATE_QUERY)){
+                    preparedStatement.setString(1, model.getUserRoleName());
+                    preparedStatement.setLong(2, model.getId());
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
     }
 
     @Override
     public void delete(UserRole model) throws SQLException {
-
+        try (Connection conn = DbUtil.getConn()) {
+            if(model.getId() != null){
+                try (PreparedStatement preparedStatement = conn.prepareStatement(DELETE_QUERY)){
+                    preparedStatement.setLong(1, model.getId());
+                    preparedStatement.executeUpdate();
+                    model.setId(null);
+                }
+            }
+        }
     }
 
     @Override
     public List<UserRole> getAll() throws SQLException {
-        return null;
+        try (Connection conn = DbUtil.getConn()) {
+            ArrayList<UserRole> userRoleList = new ArrayList<>();
+            try (ResultSet resultSet = conn.createStatement().executeQuery(GET_ALL_QUERY)) {
+                while (resultSet.next()) {
+                    UserRole userRole = new UserRole();
+                    userRole.setId(resultSet.getLong("id"));
+                    userRole.setUserRoleName(resultSet.getString("userRoleName"));
+                    userRoleList.add(userRole);
+                }
+            }
+            return userRoleList;
+        }
     }
 
     @Override
     public UserRole getById(int id) throws SQLException {
-        return null;
+        try (Connection conn = DbUtil.getConn()) {
+            UserRole loadedUserRole = new UserRole();
+            try (PreparedStatement preparedStatement = conn.prepareStatement(GET_BY_ID)){
+                preparedStatement.setLong(1, id);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()){
+                    loadedUserRole.setId(resultSet.getLong("id"));
+                    loadedUserRole.setUserRoleName(resultSet.getString("userRoleName"));
+                }
+            }
+            return loadedUserRole;
+        }
     }
 
 }
