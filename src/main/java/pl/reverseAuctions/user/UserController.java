c221f7f0f012f6e5fa687d474727fe27f90d3436
Rafał Kuszaj <rb.kuszaj@gmail.com>
2018-08-27 00:55:31 +0200

    Completed change jsp to thymeleaf. Deleted old catalog webapp. Added messages properties.

--- src/main/java/pl/reverseAuctions/user/UserController.java
+++ src/main/java/pl/reverseAuctions/user/UserController.java
@@ -2,13 +2,31 @@ package pl.reverseAuctions.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import pl.reverseAuctions.category.Category;
+import pl.reverseAuctions.category.CategoryService;
+import pl.reverseAuctions.subcategory.Subcategory;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class UserController {
 
+    private final CategoryService categoryService;
+
+    public UserController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/signIn")
     public String signIn() {
 
-        return "signIn";
+        return "signInPage";
+    }
+
+    @ModelAttribute("subcategoriesMap")
+    private Map<Category, List<Subcategory>> categoryListMap() {
+        return categoryService.getAllCategoriesWithSubcategories();
     }
 }
